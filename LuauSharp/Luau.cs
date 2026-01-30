171758745e15f94fe9c25d2eb0d3496be16d1cbe
joshuatic_ <murrjoshua505@gmail.com>
2026-01-30 08:05:53 -0600

    Refactor lua_State and lua_CompileOptions structures

--- LuauSharp/Luau.cs
+++ LuauSharp/Luau.cs
@@ -1,4 +1,4 @@
-﻿using System.Runtime.InteropServices;
+using System.Runtime.InteropServices;
 
 namespace LuauSharp;
 
@@ -14,38 +14,29 @@ internal unsafe class Luau
     private const string VM = "libLuau.VM.so";
     private const string COMPILER = "libLuau.Compiler.so";
 #endif
-    
+
     public const int LUA_MULTRET = -1;
     public const int LUAI_MAXCSTACK = 8000;
     public const int LUA_REGISTRYINDEX = -LUAI_MAXCSTACK - 2000;
     public const int LUA_ENVIRONINDEX = -LUAI_MAXCSTACK - 2001;
     public const int LUA_GLOBALSINDEX = -LUAI_MAXCSTACK - 2002;
 
-    #region Enums
-
     public enum lua_Type
     {
         LUA_TNIL = 0,
         LUA_TBOOLEAN = 1,
-
-
         LUA_TLIGHTUSERDATA,
         LUA_TNUMBER,
         LUA_TVECTOR,
-
         LUA_TSTRING,
-
-
         LUA_TTABLE,
         LUA_TFUNCTION,
         LUA_TUSERDATA,
         LUA_TTHREAD,
         LUA_TBUFFER,
-        
         LUA_TPROTO,
         LUA_TUPVAL,
         LUA_TDEADKEY,
-        
         LUA_T_COUNT = LUA_TPROTO
     };
 
@@ -60,10 +51,6 @@ internal unsafe class Luau
         LUA_BREAK,
     }
 
-    #endregion
-
-    #region Structs
-
     [StructLayout(LayoutKind.Sequential)]
     public struct lua_State
     {
@@ -72,10 +59,8 @@ internal unsafe class Luau
         public byte memcat;
         public byte status;
         public byte activememcat;
-        [MarshalAs(UnmanagedType.I1)]
-        public bool isactive;
-        [MarshalAs(UnmanagedType.I1)]
-        public bool singlestep;
+        [MarshalAs(UnmanagedType.I1)] public bool isactive;
+        [MarshalAs(UnmanagedType.I1)] public bool singlestep;
 
         public IntPtr top;
         public IntPtr @base;
@@ -100,7 +85,7 @@ internal unsafe class Luau
         public IntPtr namecall;
         public IntPtr userdata;
     }
-    
+
     [StructLayout(LayoutKind.Sequential)]
     public struct lua_CompileOptions
     {
@@ -116,143 +101,13 @@ internal unsafe class Luau
         public IntPtr mutableGlobals;
         public IntPtr userdataTypes;
 
+        // FIXED: assign fields, not locals
         public lua_CompileOptions()
         {
-            int optimizationLevel = 1;
-            int debugLevel = 1;
-            int typeInfoLevel = 0;
-            int coverageLevel = 0;
+            optimizationLevel = 1;
+            debugLevel = 1;
+            typeInfoLevel = 0;
+            coverageLevel = 0;
         }
     }
-
-    #endregion
-    
-    #region Native Delegates
-    
-    [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
-    public delegate int LuaCFunction(lua_State* L);
-
-    [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
-    public delegate int LuaContinuation(lua_State* L, int status);
-    
-    #endregion
-
-    #region Helper Functions
-
-    public static int lua_upvalueindex(int i) => LUA_GLOBALSINDEX - i;
-    public static bool lua_isboolean(lua_State* L, int idx) => lua_type(L, idx) == (int) lua_Type.LUA_TBOOLEAN;
-    public static bool lua_isnil(lua_State* L, int idx) => lua_type(L, idx) == (int) lua_Type.LUA_TNIL;
-    public static bool lua_isfunction(lua_State* L, int idx) => lua_type(L, idx) == (int) lua_Type.LUA_TFUNCTION;
-
-    #endregion
-    
-    #region StateControl
-    
-    [DllImport(VM, CallingConvention = CallingConvention.Cdecl)]
-    public static extern lua_State* luaL_newstate();
-    
-    [DllImport(VM, CallingConvention = CallingConvention.Cdecl)]
-    public static extern void lua_setsafeenv(lua_State* L, int index, int value);
-
-    [DllImport(VM, CallingConvention = CallingConvention.Cdecl)]
-    public static extern void luaL_openlibs(lua_State* L);
-    
-    [DllImport(VM, CallingConvention = CallingConvention.Cdecl)]
-    public static extern void lua_close(lua_State* L);
-    
-    #endregion
-
-    #region Loading
-
-    [DllImport(COMPILER, CharSet = CharSet.Ansi, CallingConvention = CallingConvention.Cdecl)]
-    public static extern IntPtr luau_compile(string source, IntPtr size, IntPtr options, out IntPtr outsize);
-
-    [DllImport(VM, CallingConvention = CallingConvention.Cdecl)]
-    public static extern int luau_load(lua_State* L, string name, byte[] bytecode, long size, int flags);
-
-    #endregion
-
-    #region Type Conversion
-    
-    [DllImport(VM, CallingConvention = CallingConvention.Cdecl)]
-    public static extern int lua_isnumber(lua_State* L, int idx);
-    
-    [DllImport(VM, CallingConvention = CallingConvention.Cdecl)]
-    public static extern int lua_isstring(lua_State* L, int idx);
-    
-    [DllImport(VM, CallingConvention = CallingConvention.Cdecl)]
-    public static extern int lua_isuserdata(lua_State* L, int idx);
-
-    [DllImport(VM, CallingConvention = CallingConvention.Cdecl)]
-    public static extern int lua_type(lua_State* L, int idx);
-
-    [DllImport(VM, CallingConvention = CallingConvention.Cdecl)]
-    public static extern IntPtr lua_tolstring(lua_State* L, int idx, out IntPtr len);
-    
-    [DllImport(VM, CallingConvention = CallingConvention.Cdecl)]
-    public static extern int lua_tointegerx(lua_State* L, int idx, out int isnum);
-    
-    [DllImport(VM, CallingConvention = CallingConvention.Cdecl)]
-    public static extern double lua_tonumberx(lua_State* L, int idx, out int isnum);
-
-    [DllImport(VM, CallingConvention = CallingConvention.Cdecl)]
-    public static extern int lua_toboolean(lua_State* L, int idx);
-    
-    [DllImport(VM, CallingConvention = CallingConvention.Cdecl)]
-    public static extern IntPtr lua_tolightuserdatatagged(lua_State* luaState, int idx, int tag);
-
-    [DllImport(VM, CallingConvention = CallingConvention.Cdecl)]
-    public static extern IntPtr lua_tocfunction(lua_State* luaState, int idx);
-
-    #endregion
-
-    #region Functions
-
-    [DllImport(VM, CallingConvention = CallingConvention.Cdecl)]
-    public static extern int lua_pcall(lua_State* L, int nargs, int nresults, int msgh);
-    
-    [DllImport(VM, CallingConvention = CallingConvention.Cdecl)]
-    public static extern void lua_setfield(lua_State* L, int idx, string k);
-
-    [DllImport(VM, CallingConvention = CallingConvention.Cdecl)]
-    public static extern int lua_setmetatable(lua_State* L, int objindex);
-
-    [DllImport(VM, CallingConvention = CallingConvention.Cdecl)]
-    public static extern int lua_gettop(lua_State* L);
-
-    [DllImport(VM, CallingConvention = CallingConvention.Cdecl)]
-    public static extern void lua_call(lua_State* L, int nargs, int nresults);
-
-    #endregion
-
-    #region Variable Pushing
-
-    [DllImport(VM, CallingConvention = CallingConvention.Cdecl)]
-    public static extern void lua_createtable(lua_State* L, int narr, int n);
-    
-    [DllImport(VM, CallingConvention = CallingConvention.Cdecl)]
-    public static extern void lua_pushlightuserdatatagged(lua_State* luaState, IntPtr p, int tag);
-    
-    [DllImport(VM, CallingConvention = CallingConvention.Cdecl)]
-    public static extern void lua_pushcclosurek(lua_State* L, LuaCFunction fn, string debugname, int nup, LuaContinuation cont);
-
-    [DllImport(VM, CallingConvention = CallingConvention.Cdecl)]
-    public static extern void lua_pushnil(lua_State* L);
-    
-    [DllImport(VM, CallingConvention = CallingConvention.Cdecl)]
-    public static extern void lua_pushboolean(lua_State* L, int b);
-    
-    [DllImport(VM, CallingConvention = CallingConvention.Cdecl)]
-    public static extern void lua_pushstring(lua_State* L, string s);
-    
-    [DllImport(VM, CallingConvention = CallingConvention.Cdecl)]
-    public static extern void lua_pushinteger(lua_State* L, int n);
-    
-    [DllImport(VM, CallingConvention = CallingConvention.Cdecl)]
-    public static extern void lua_pushnumber(lua_State* L, double n);
-
-    [DllImport(VM, CallingConvention = CallingConvention.Cdecl)]
-    public static extern void lua_pushvalue(lua_State* L, int idx);
-
-    #endregion
 }
